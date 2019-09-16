c7f73ffdbffb1d825777c0aec646b535b1598c64
escapemanuele <e.buccelli@hotmail.it>
2019-09-16 15:50:09 +0200

    fix affiliate

--- src/components/Globals/AffiliateText.js
+++ src/components/Globals/AffiliateText.js
@@ -1,62 +1,64 @@
 import React from 'react';
 import styled from 'styled-components'
 import {styles} from '../../utils'
-import { FaArrowRight, FaArrowDown } from 'react-icons/fa'
+import { FaArrowDown } from 'react-icons/fa'
 
 const AffiliateText = (link) => (
   <AffiliateWrapper>
-    <p>
-      Ready to host your new project?<br/>
-    </p>
-    <FaArrowRight className="arrow-right" />
-    <FaArrowDown className="arrow-down" />
-    <a className="affiliate_link" href="https://www.siteground.com/go/debuxing">
-      SITEGROUND 
+    <a href="https://www.siteground.com/go/debuxing">
+      <p>
+        Ready to host your new project?<br/>
+      </p>
+      <div className="affiliate_link" href="https://www.siteground.com/go/debuxing">
+        <FaArrowDown />
+        <p>SITEGROUND</p>
+      </div>
     </a>
   </AffiliateWrapper>
 );
 
 const AffiliateWrapper = styled.div`
 
-  display: block;
   text-align: center;
   margin-bottom: 1.2rem;
   margin-top:1.2rem;
-  padding: 0.8rem;
-  width: 40vw;
+  
+  width: 80vw;
+  
   margin-left: auto;
   margin-right: auto;
   color: ${styles.colors.mainBlack};
-  ${styles.border({color:`${styles.colors.mainBlue}`})};
+  ${styles.border({color: `${styles.colors.mainBlue}`})};
 
-  .arrow-right {
-    visibility: hidden;
-    display: none;
-  }
-  .arrow-down {
-    visibility: visible;
+  a {
+    display: block;
+    width: 100%;
+    padding:0.5rem;
+    font-size: 1.2rem;
+    font-weight: 700;
+    ${styles.transDefault};
+
+    &:hover {
+      background: ${styles.colors.mainBlue};
+
+      .affiliate_link {
+        color: ${styles.colors.mainWhite};
+        font-size: 1.8rem;
+      }
+    }
   }
 
   .affiliate_link {
     display: block;
+    align-items: center;
     font-weight: 900;
-    font-size: 1.2rem;
+    font-size: 1.6rem;
     color: ${styles.colors.mainBlue};
+    ${styles.transDefault};
   }
 
    @media(min-width: ${styles.size.tablet}) {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-
-    .arrow-down {
-    visibility: hidden;
-    }
-    .arrow-right {
-      visibility: visible;
-      display: inline-block;
-    }
-
+      width: 40vw;
    }
 
 `
